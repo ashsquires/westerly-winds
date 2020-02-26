f36b08ddd0395a1464ef6001d09be808cbdddce2
Ashton Squires <ashton@silverwing.co>
2020-02-26 16:59:51 +0000

    new 2

---
+++ mainjq.js
@@ -0,0 +1,19 @@
+$(document).ready(() => {
+
+  $('#topim').animate({
+    'marginTop' : '-=40px',
+    'opacity' : '1'
+  }, 1200);
+  console.log('ready');
+
+  $('#contact').delay(1000).animate({
+    'opacity' : '1'
+  }, 1200);
+  console.log('ready');
+
+
+
+
+
+
+});
